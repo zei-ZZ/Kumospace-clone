58006d8946e8b3bd7b2ae0b7d378c8bf0339cc37
Donia Bougrine <124002250+doniab7@users.noreply.github.com>
2025-01-23 20:38:07 +0100

    fix auth component

--- frontend/src/app/auth/auth.component.ts
+++ frontend/src/app/auth/auth.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  AbstractControlOptions,
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
   imports: [ReactiveFormsModule],
   templateUrl: './auth.component.html',
-  styleUrl: './auth.component.css'
+  styleUrl: './auth.component.css',
 })
 export class AuthComponent {
   isSignUp = false;
@@ -15,40 +23,50 @@ export class AuthComponent {
   constructor(private fb: FormBuilder) {
     this.signInForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required]]
+      password: ['', [Validators.required]],
     });
 
-    this.signUpForm = this.fb.group({
-      name: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(6)]],
-      confirmPassword: ['', Validators.required]
-    }, { validator: this.passwordMatchValidator });
+    const signUpOptions: AbstractControlOptions = {
+      validators: this.passwordMatchValidator(),
+    };
+
+    this.signUpForm = this.fb.group(
+      {
+        username: ['', Validators.required],
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', [Validators.required, Validators.minLength(6)]],
+        confirmPassword: ['', Validators.required],
+      },
+      signUpOptions
+    );
   }
 
   toggleForm() {
     this.isSignUp = !this.isSignUp;
   }
 
-  passwordMatchValidator(form: FormGroup) {
-    const password = form.get('password');
-    const confirmPassword = form.get('confirmPassword');
-    
-    return password && confirmPassword && password.value === confirmPassword.value 
-      ? null 
-      : { passwordMismatch: true };
+  passwordMatchValidator(): ValidatorFn {
+    return (control: AbstractControl) => {
+      const group = control as FormGroup;
+      const password = group.get('password');
+      const confirmPassword = group.get('confirmPassword');
+
+      return password &&
+        confirmPassword &&
+        password.value === confirmPassword.value
+        ? null
+        : { passwordMismatch: true };
+    };
   }
 
   onSignIn() {
     if (this.signInForm.valid) {
-      // Implement sign-in logic
       console.log(this.signInForm.value);
     }
   }
 
   onSignUp() {
     if (this.signUpForm.valid) {
-      // Implement sign-up logic
       console.log(this.signUpForm.value);
     }
   }
