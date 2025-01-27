02a401a0f26f14a864cb03cb6a43d2f3e34faca0
chamsTmar1 <chamstmar@gmail.com>
2025-01-27 09:35:37 +0100

    ok

--- frontend/src/app/layout/layout.component.ts
+++ frontend/src/app/layout/layout.component.ts
@@ -1,4 +1,4 @@
-import { Component, Signal, signal, computed, effect, OnInit, OnDestroy } from '@angular/core';
+import { Component, signal, computed, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-layout',
@@ -6,8 +6,7 @@ import { Component, Signal, signal, computed, effect, OnInit, OnDestroy } from '
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css',
 })
-export class LayoutComponent implements OnInit, OnDestroy{
-
+export class LayoutComponent implements OnInit, OnDestroy {
   ngOnInit() {
     window.addEventListener('keydown', (event) => this.handleKeyPress(event));
   }
@@ -19,10 +18,10 @@ export class LayoutComponent implements OnInit, OnDestroy{
       ArrowLeft: 'left',
       ArrowRight: 'right',
     } as const;
-  
+
     // I added this line due to TS strict typing that doesn't accept keyMap[event.key]
     const direction = keyMap[event.key as keyof typeof keyMap];
-    
+
     if (direction) {
       this.moveCharacter(direction);
     }
@@ -32,27 +31,34 @@ export class LayoutComponent implements OnInit, OnDestroy{
   charX = signal(0);
   charY = signal(0);
 
-  // Camera's position depending on character's position
-  cameraX = computed(() => Math.max(0, this.charX() - window.innerWidth / 2));
-  cameraY = computed(() => Math.max(0, this.charY() - window.innerHeight / 2));
+  // Map's position depending on character's position
+  mapX = computed(() => Math.max(0, this.charX() - window.innerWidth / 2));
+  mapY = computed(() => Math.max(0, this.charY() - window.innerHeight / 2));
 
   moveCharacter(direction: 'up' | 'down' | 'left' | 'right') {
-    const step = 10; // Movement speed (we'll adjust it later on)
+    const step = 30; // Movement speed (we'll adjust it later on)
 
     switch (direction) {
       /**
        * In CSS and the DOM, the coordinate system for positioning elements works differently than Cartesian system
        *  so moving up decreases y, moving down increases y, (x stays the same 😌)
-      **/
-      case 'up': this.charY.update((y) => y - step); break; 
-      case 'down': this.charY.update((y) => y + step); break;
-      case 'left': this.charX.update((x) => x - step); break;
-      case 'right': this.charX.update((x) => x + step); break;
+       **/
+      case 'up':
+        this.charY.update((y) => Math.max(y - step, 0));
+        break;
+      case 'down':
+        this.charY.update((y) => y + step);
+        break;
+      case 'left':
+        this.charX.update((x) => Math.max(x - step, 0));
+        break;
+      case 'right':
+        this.charX.update((x) => x + step);
+        break;
     }
   }
-  
+
   ngOnDestroy() {
     window.removeEventListener('keydown', this.handleKeyPress);
   }
-
 }
