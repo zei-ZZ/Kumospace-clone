8c641dc9971df40aed8b3602491702465dc758f3
chamsTmar1 <chamstmar@gmail.com>
2025-01-27 11:31:03 +0100

    preventing avatar steps into edges

--- frontend/src/app/layout/layout.component.ts
+++ frontend/src/app/layout/layout.component.ts
@@ -7,8 +7,48 @@ import { Component, signal, computed, OnInit, OnDestroy } from '@angular/core';
   styleUrl: './layout.component.css',
 })
 export class LayoutComponent implements OnInit, OnDestroy {
+
+
+  // Character's position (writable signals)
+  charX = signal(0);
+  charY = signal(0);
+
+  // Map dimensions are dynamic so we need to calculate them
+  private mapWidth = signal(this.calculateMapWidth());
+  private mapHeight = signal(this.calculateMapHeight());
+
+  /**
+   * Map's position depending on character's position :
+   *
+   * Math.min(... , mapDimension - viewportSize) prevents
+   * map from exposing its background by keeping the
+   * bottom-right corner visible
+   *
+   * Math.max(0, ...) prevents map from moving into neg coords
+   **/
+
+  mapX = computed(() =>
+    Math.min(
+      Math.max(0, this.charX() - window.innerWidth / 2),
+      this.mapWidth() - window.innerWidth
+    )
+  );
+  mapY = computed(() =>
+    Math.min(
+      Math.max(0, this.charY() - window.innerHeight / 2),
+      this.mapHeight() - window.innerHeight
+    )
+  );
+
   ngOnInit() {
+    this.updateMapDimensions();
+    window.addEventListener('resize', this.updateMapDimensions.bind(this));
     window.addEventListener('keydown', (event) => this.handleKeyPress(event));
+  }
+
+  ngOnDestroy() {
+    window.removeEventListener('resize', this.updateMapDimensions.bind(this));
+    window.removeEventListener('keydown', this.handleKeyPress);
   }
 
   private handleKeyPress(event: KeyboardEvent) {
@@ -27,14 +67,6 @@ export class LayoutComponent implements OnInit, OnDestroy {
     }
   }
 
-  // Character's position (writable signals)
-  charX = signal(0);
-  charY = signal(0);
-
-  // Map's position depending on character's position
-  mapX = computed(() => Math.max(0, this.charX() - window.innerWidth / 2));
-  mapY = computed(() => Math.max(0, this.charY() - window.innerHeight / 2));
-
   moveCharacter(direction: 'up' | 'down' | 'left' | 'right') {
     const step = 30; // Movement speed (we'll adjust it later on)
 
@@ -47,18 +79,57 @@ export class LayoutComponent implements OnInit, OnDestroy {
         this.charY.update((y) => Math.max(y - step, 0));
         break;
       case 'down':
-        this.charY.update((y) => y + step);
+        this.charY.update((y) =>
+          Math.min(y + step, this.mapHeight() - this.characterSize())
+        );
         break;
       case 'left':
         this.charX.update((x) => Math.max(x - step, 0));
         break;
       case 'right':
-        this.charX.update((x) => x + step);
+        this.charX.update((x) =>
+          Math.min(x + step, this.mapWidth() - this.characterSize())
+        );
         break;
     }
   }
 
-  ngOnDestroy() {
-    window.removeEventListener('keydown', this.handleKeyPress);
+  private calculateMapWidth(): number {
+    const vw = window.innerWidth;
+    if (vw >= 1000) {
+      return vw * 1.2; // 120vw -> large screens
+    } else if (vw >= 700) {
+      return vw * 1.5; // 150vw -> medium screens
+    } else {
+      return vw * 1.8; // 180vw -> small screens
+    }
   }
+
+  private calculateMapHeight(): number {
+    const vh = window.innerHeight;
+    if (window.innerWidth >= 1000) {
+      return vh * 1.2;
+    } else if (window.innerWidth >= 700) {
+      return vh * 1.5;
+    } else {
+      return vh * 1.8;
+    }
+  }
+
+  private characterSize(): number {
+    const vw = window.innerWidth;
+    if (vw >= 1000) {
+      return vw * 0.05; // 5vw -> large screens
+    } else if (vw >= 700) {
+      return vw * 0.08;
+    } else {
+      return vw * 0.1;
+    }
+  }
+
+  private updateMapDimensions() {
+    this.mapWidth.set(this.calculateMapWidth());
+    this.mapHeight.set(this.calculateMapHeight());
+  }
+
 }
