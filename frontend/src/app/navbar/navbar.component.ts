fd7049df0449fdd853b186ae4cb24ecafba6c875
WindyJasmin <123154941+WindyJasmin@users.noreply.github.com>
2025-01-28 12:15:47 +0100

    login button redirects to auth page

---
+++ frontend/src/app/navbar/navbar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { ImageModule } from 'primeng/image';
+@Component({
+  selector: 'app-navbar',
+  imports: [],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css',
+  standalone: true ,
+})
+export class NavbarComponent {}
