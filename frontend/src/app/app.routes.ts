ac5244d0539e8a817b94b23505aa100ba5e56e7f
chamsTmar1 <chamstmar@gmail.com>
2025-01-28 11:26:09 +0100

    merging with master

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,7 +3,6 @@ import { AuthComponent } from './auth/auth.component';
 import { UserPageComponent } from './user-page/user-page.component';
 import { authGuard } from './shared/guards/auth.guard';
 import { LayoutComponent } from './layout/layout.component';
-import { SpaceComponent } from './space/space.component';
 
 export const routes: Routes = [
   {
@@ -19,8 +18,4 @@ export const routes: Routes = [
     path: 'layout',
     component: LayoutComponent
   },
-  {
-    path: 'space',
-    component: SpaceComponent
-  }
 ];
