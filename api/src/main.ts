f2e3b9c83262b61ca7b826564e579952c1a779cd
FarahBelHajAmmmar <farah.belhajammar@insat.ucar.tn>
2025-01-28 12:22:05 +0100

    .

--- api/src/main.ts
+++ api/src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: true,
+    origin: 'http://localhost:4200', 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: ['Content-Type', 'Accept', 'Authorization'],
     credentials: true,
