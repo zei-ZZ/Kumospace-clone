0beda300bb5bff05da5f6916b8664bafc8b28f4e
FarahBelHajAmmmar <farah.belhajammar@insat.ucar.tn>
2025-01-28 12:10:54 +0100

    .

--- api/src/chat/chat.gateway.ts
+++ api/src/chat/chat.gateway.ts
@@ -30,12 +30,14 @@ export class ChatGateway implements OnGatewayInit, OnGatewayConnection, OnGatewa
 
   // Méthode pour envoyer un message dans une room
   @SubscribeMessage('sendMessage')
-  handleMessage(client: Socket, payload: { roomId: string, message: string }): void {
-    this.server.to(payload.roomId).emit('message', {
-      userId: client.id,
-      message: payload.message,
-    });
-  }
+handleMessage(client: Socket, payload: { roomId: string, message: string }): void {
+  console.log('Message received:', payload); // Ajouter ce log pour vérifier
+  this.server.to(payload.roomId).emit('message', {
+    userId: client.id,
+    message: payload.message,
+  });
+}
+
 
   // Méthode pour quitter une room
   @SubscribeMessage('leaveRoom')
