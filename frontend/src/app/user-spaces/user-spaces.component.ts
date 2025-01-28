b402f01cea6c13c77cd21d17340899aca76d24bf
Donia Bougrine <124002250+doniab7@users.noreply.github.com>
2025-01-28 19:11:02 +0100

    list of spaces fix css

--- frontend/src/app/user-spaces/user-spaces.component.ts
+++ frontend/src/app/user-spaces/user-spaces.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Space } from '../shared/models/space';
 
 @Component({
   selector: 'app-user-spaces',
@@ -7,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrl: './user-spaces.component.css'
 })
 export class UserSpacesComponent {
+  spaces: Space[] = [];
+
+  ngOnInit(): void {
+    this.spaces = this.getDummySpaces();
+    console.log(this.spaces);
+  }
+
+  getDummySpaces(): Space[] {
+    return [
+      {
+        id: '1',
+        name: 'Space 1',
+        key: 'key1',
+        capacity: 10,
+        userId: 'user1'
+      },
+    ];
+  }
 
 }
